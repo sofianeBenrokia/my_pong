d686363b131012967e6309b0a37cd7d54dcae232
sofiane benrokia <benrokia.sofiane.adel@gmail.com>
2021-12-16 17:47:29 +0100

    16/12/2021 - 17:47

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -2,6 +2,11 @@
 *   program written by sofiane benrokia
 *   email: benrokia.sofiane.adel@gmail.com
 *   github: https://github.com/sofianeBenrokia/my_pong
+*   
+*   the version of SDL2 used is the 2.0.16
+*   don't forget to add the SDL2 library in main\dependence\SDL2-2.0.16 (visual studio) 
+*   and move the dll file in Win32\Debug or x64\Debug depending on the version you are using or
+*   Win32\release or x64\release if you are in the release mode.
 */
 #include <windows.h>
 #include <SDL.h>
